cb0ceeb5f1e9c353d1033f36a7d5f112028c88cd
Milena <milimaalc.dev@gmail.com>
2025-04-23 17:55:04 -0300

    add service

---
+++ CineHarbor/Controllers/TrendingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrendingViewController.swift
+//  CineHarbor
+//
+//  Created by Milena on 23/04/2025.
+//
+
+import UIKit
+
+class TrendingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CineHarbor/Views/trendingCell.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  CineHarbor
+//
+//  Created by Milena on 23/04/2025.
+//
+
